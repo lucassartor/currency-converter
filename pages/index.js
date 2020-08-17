2cd5e801af88074b60d797694f542dec6e3deec7
lucassartor <49413772+lucassartor@users.noreply.github.com>
2020-08-16 21:24:36 -0300

    add footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import Head from "next/head";
 import axios from 'axios';
+import {BsFillHeartFill, BsArrowUpDown} from "react-icons/bs";
+import {IconContext} from "react-icons";
+
 
 let timeout = 0;
 const apiKey = "f205970f3571f475aee5";
@@ -8,20 +11,19 @@ const apiKey = "f205970f3571f475aee5";
 class Input extends Component {
 
 
-    static getDerivedStateFromProps(nextProps, prevState){
-        if(prevState.secondCurrencyName === 'BRL')
-            return({
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (prevState.secondCurrencyName === 'BRL')
+            return ({
                 firstCurrency: nextProps.currencyBRL,
                 secondCurrency: nextProps.currencyUSD
             });
 
         else
-            return({
+            return ({
                 firstCurrency: nextProps.currencyUSD,
                 secondCurrency: nextProps.currencyBRL
             });
     }
-
 
     constructor(props) {
         super(props);
@@ -45,7 +47,7 @@ class Input extends Component {
         this.setState({value: event.target.value});
     }
 
-    handleSwitch(){
+    handleSwitch() {
         let newFirstCurrencyIcon = this.state.secondCurrencyIcon;
         let newFirstCurrencyName = this.state.secondCurrencyName;
         let newSecondCurrencyIcon = this.state.firstCurrencyIcon;
@@ -63,29 +65,44 @@ class Input extends Component {
 
     render() {
         return (
-            <div className="max-w-sm shadow-lg h-64 overflow-hidden shadow-lg text-lg text-center  bg-white rounded ">
-                <div className="flex items-center px-8 pt-10">
-                    <img src={this.state.firstCurrencyIcon} width="30" className="mx-auto" alt={this.state.firstCurrencyName}/>
-                    <h1 className="text-center px-2">{this.state.firstCurrencyName}:</h1>
-                    <input
-                        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-1  appearance-none leading-normal"
-                        type="text" value={this.state.value} onChange={this.handleChange}/>
-                </div>
+            <div>
+                <div className="max-w-sm shadow-lg h-64 overflow-hidden shadow-lg text-lg text-center  bg-white rounded ">
+                    <div className="flex items-center px-8 pt-10">
+                        <img src={this.state.firstCurrencyIcon} width="30" className="mx-auto"
+                             alt={this.state.firstCurrencyName}/>
+                        <h1 className="text-center px-2">{this.state.firstCurrencyName}:</h1>
+                        <input
+                            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-1  appearance-none leading-normal"
+                            type="text" value={this.state.value} onChange={this.handleChange}/>
+                    </div>
 
-                <div className="mx-auto m-4">
-                    {/*<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded" onClick={this.handleSwitch}>
-                        Switch
-                    </button>*/}
-                    <img className="mx-auto" src="/switch.png" width="40"  alt="switch" onClick={this.handleSwitch}/>
-                </div>
+                    <div className="mx-auto m-4">
+                        <IconContext.Provider value={{size: 35}}>
+                            <div>
+                                <BsArrowUpDown className="mx-auto" onClick={this.handleSwitch}/>
+                            </div>
+                        </IconContext.Provider>
+                    </div>
 
-                <div className="flex items-center px-8">
-                    <img src={this.state.secondCurrencyIcon} width="30" className="mx-auto" alt={this.state.secondCurrencyName}/>
-                    <h1 className="text-center px-2">{this.state.secondCurrencyName}:</h1>
-                    <input
-                        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-1  appearance-none leading-normal"
-                        type="text" value={parseFloat(this.state.value * this.state.secondCurrency).toFixed(2)}
-                        onChange={this.handleChange}/>
+                    <div className="flex items-center px-8">
+                        <img src={this.state.secondCurrencyIcon} width="30" className="mx-auto"
+                             alt={this.state.secondCurrencyName}/>
+                        <h1 className="text-center px-2">{this.state.secondCurrencyName}:</h1>
+                        <input
+                            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-1  appearance-none leading-normal"
+                            type="text" value={parseFloat(this.state.value * this.state.secondCurrency).toFixed(2)}
+                            onChange={this.handleChange}/>
+                    </div>
+
+                    <div className="mx-auto ml-6 flex px-16 pt-6 items-center text-sm">
+                        Made with
+                        <IconContext.Provider value={{color: "red"}}>
+                            <div className="mx-1">
+                                <BsFillHeartFill/>
+                            </div>
+                        </IconContext.Provider>
+                        by <a className="mx-1" href="https://github.com/lucassartor">Lucas Sartor</a>
+                    </div>
                 </div>
             </div>
         );
@@ -103,7 +120,7 @@ class Home extends Component {
         };
 
         this.fetchCurrency = this.fetchCurrency.bind(this);
-        this.handleCurrency = this.handleCurrency.bind(this);
+        this.setCurrency = this.setCurrency.bind(this);
     }
 
 
@@ -112,13 +129,13 @@ class Home extends Component {
             const response = await axios.get("https://free.currconv.com/api/v7/convert?q=" + firstCurrency + "_" + secondCurrency + "," + secondCurrency + "_" + firstCurrency + "&compact=ultra&apiKey=" + apiKey);
             const values = await response.data;
 
-            this.handleCurrency(values)
+            this.setCurrency(values)
         }, timeout);
 
     }
 
 
-    handleCurrency(value) {
+    setCurrency(value) {
         this.setState({
             currencyUSD: value.USD_BRL,
             currencyBRL: value.BRL_USD
